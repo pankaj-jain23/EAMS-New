cf046c1fd3c3e3b1333993ef446b1a6cd574ce11
MPC123-SP <125030723+MPC123-SP@users.noreply.github.com>
2023-11-24 14:28:37 +0530

    pcid added in addassembly
    pcid added addassembly

--- EAMS/ViewModels/AssemblyMasterViewModel.cs
+++ EAMS/ViewModels/AssemblyMasterViewModel.cs
@@ -7,6 +7,7 @@
         public int AssemblyCode { get; set; }
         public int StateMasterId { get; set; }
         public int DistrictMasterId { get; set; }
+        public int PCMasterId { get; set; }
         public string? AssemblyType { get; set; }
         public bool Status { get; set; } = true;
         //public List<BoothMasterViewModel>boothMasterViewModels { get; set; }
